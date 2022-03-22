f025c9d342476b180288928dacd76d7cb2300d51
gamegtboyz <102132550+gamegtboyz@users.noreply.github.com>
2022-03-22 15:45:07 +0700

    Add files via upload

---
+++ chapter2/Date.java
@@ -0,0 +1,15 @@
+package chapter2;
+
+public class Date {
+    public static void main(String[] data) {
+        // create program that creates variables named day, date, month, year. Assign values to those variables that represent today's date.
+        String day = "Tuesday";
+        int date = 22;
+        String month = "March";
+        int year = 2022;
+
+        System.out.println("Today in American format is " + day + ", " + month + " " + date + ", " + year + ".");
+        System.out.println("Today in European format is " + day + " " + date + " " + month + " " + year + ".");
+
+    }
+}
