a5593011918c93b1eee69f06d9f0a37a04fa0212
gamegtboyz <102132550+gamegtboyz@users.noreply.github.com>
2022-03-22 13:43:29 +0700

    Add files via upload

---
+++ chapter1.java
@@ -0,0 +1,27 @@
+public class chapter1 {
+    public static void main(String[] args) {
+        /*
+        Excercise 1.1: know the jargons.
+            1. In computer jargon, what's the difference between a 'statement' and a 'comment'?
+                >>> In computer jargon, 'statement' means the line of code that performs the simple action e.g. System.out.println(); which is the print statement. On the other hand 'comments' is used to describe the written statements, but have no effect to executioning the program.
+            2. What does it mean to say that a program is 'portable'?
+                >>> the "portable" program means that the written program could be executed regardless of the machine that it is running on. The portable program is usually featured in high-level computer language e.g. Java, Python, etc.
+            3. In common English, what does the word compile mean?
+                >>> 'compile' is the computer process that translates the entire code written in high-level language on to the bytecode which is executable by the machine.
+            4. What is an 'executable'? Why is that word used as a noun?
+                >>> the 'executable' is used to describe the compiled source code which could be run by the machine without further modification of the code. Please note that the 'executable' file of each source code is vary machine by machine.
+        */
+
+        //Exercise 1.2: Find out how to compile and run on Java
+            System.out.println("Exercise 1.2:");
+            //1.1 Type in the Hello world program; then compile and run it
+                System.out.println("Hello World!");
+
+            //1.2 Add a print statement that displays a second message after the Hello world!
+                System.out.println("We try to print this one and it works!");
+
+            //1.3 We try to add this comment and hope that it will not affect the code.
+
+        //Exercise 1.3: this one is about try to make some errors. I have no code shows here, but it'll make the entire code unexecutable.
+    }
+}
